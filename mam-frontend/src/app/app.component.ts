38fec6320d9be16097f0493567005158e3312552
wael2000 <weldoami@redhat.com>
2023-06-09 15:51:47 +0400

    wip:before updating

--- mam-frontend/src/app/app.component.ts
+++ mam-frontend/src/app/app.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { KeycloakProfile } from 'keycloak-js';
 
+import { TranslationService } from './modules/i18n';
+// language list
+import { locale as enLang } from './modules/i18n/vocabs/en';
+import { locale as chLang } from './modules/i18n/vocabs/ch';
+import { locale as esLang } from './modules/i18n/vocabs/es';
+import { locale as jpLang } from './modules/i18n/vocabs/jp';
+import { locale as deLang } from './modules/i18n/vocabs/de';
+import { locale as frLang } from './modules/i18n/vocabs/fr';
+import { ThemeModeService } from './_metronic/partials/layout/theme-mode-switcher/theme-mode.service';
+
+
 @Component({
-  selector: 'app-root',
+  //selector: 'app-root',
+  selector: 'body[root]',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mam-frontend';
   public isLoggedIn = false;
   public userProfile: KeycloakProfile | null = null;
 
-  constructor(private readonly keycloak: KeycloakService) {}
+  constructor(private readonly keycloak: KeycloakService,
+              private translationService: TranslationService,
+              private modeService: ThemeModeService) {
+              // register translations
+    this.translationService.loadTranslations(
+      enLang,
+      frLang
+    );
+  }
 
   public async ngOnInit() {
+    this.modeService.init();
     this.isLoggedIn = await this.keycloak.isLoggedIn();
-
     if (this.isLoggedIn) {
       this.userProfile = await this.keycloak.loadUserProfile();
     }
--- mam-frontend/src/app/app.module.ts
+++ mam-frontend/src/app/app.module.ts
@@ -8,6 +8,19 @@ import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ClipboardModule } from 'ngx-clipboard';
+import { TranslateModule } from '@ngx-translate/core';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from './modules/auth/services/auth.service';
+import { environment } from 'src/environments/environment';
+// #fake-start#
+import { FakeAPIService } from './_fake/fake-api.service';
+// #fake-end#
+
 function initializeKeycloak(keycloak: KeycloakService) {
   return () =>
     keycloak.init({
@@ -34,7 +47,20 @@ function initializeKeycloak(keycloak: KeycloakService) {
     BrowserModule,
     AppRoutingModule,
     KeycloakAngularModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot(),
+    ClipboardModule,
+    // #fake-start#
+    environment.isMockEnabled
+    ? HttpClientInMemoryWebApiModule.forRoot(FakeAPIService, {
+        passThruUnknownUrl: true,
+        dataEncapsulation: false,
+      })
+    : [],
+    // #fake-end#
+    InlineSVGModule.forRoot(),
+    NgbModule,
   ],
   providers: [
     {
